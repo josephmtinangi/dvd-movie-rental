31daa427b8e31b85bfc8378becf69ab94cdb1360
Joseph Mtinangi <josephmtinangi@gmail.com>
2023-05-17 14:58:26 +0800

    Prepare for deployment

--- moviedvdrental/settings.py
+++ moviedvdrental/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ncz-1(qe5r+435_3yea5-$j*w86+#f%2q#rmffe^uw&f^!a**z'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-ncz-1(qe5r+435_3yea5-$j*w86+#f%2q#rmffe^uw&f^!a**z')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
