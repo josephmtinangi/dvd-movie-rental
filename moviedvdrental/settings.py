f1b74d88f1257750a3b223084288168daceea65e
Joseph Mtinangi <josephmtinangi@gmail.com>
2023-05-21 10:10:19 +0800

    Add list of allowed hosts

--- moviedvdrental/settings.py
+++ moviedvdrental/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-ncz-1(qe5r+435
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'josephmtinangi.pythonanywhere.com']
 
 
 # Application definition
